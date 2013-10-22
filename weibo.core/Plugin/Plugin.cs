6540faf86fe95ac9b3f229e489114ae425a0578c
zyouhua <zyouhua@gmail.com>
2013-10-22 16:01:51 +0800

    update

---
+++ weibo.core/Plugin/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using platform;
+using account.core;
+
+namespace weibo.core
+{
+    public class Plugin : IPlugin
+    {
+        public void _runLoad()
+        {
+            AccountCreator accountCreator_ = __singleton<AccountCreator>._instance();
+            accountCreator_._registerCreate(new PropertyId<StatusMgr>());
+            
+            StatusService statusService_ = __singleton<StatusService>._instance();
+            statusService_._runPreinit();
+
+            AccountService accountService_ = __singleton<AccountService>._instance();
+            accountService_._registerCreate(statusService_);
+        }
+    }
+}
