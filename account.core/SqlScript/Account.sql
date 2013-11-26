467c024278f09ab514e0fb8e0f9f707cc6447772
zyouhua <zyouhua@gmail.com>
2013-11-26 09:58:56 +0800

    update

--- account.core/SqlScript/Account.sql
+++ account.core/SqlScript/Account.sql
@@ -16,11 +16,12 @@ DROP PROCEDURE IF EXISTS `account`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `account`()
 BEGIN
 	DECLARE i INT DEFAULT 0;
-	DECLARE tableCount INT DEFAULT 10;
+	DECLARE tableCount INT DEFAULT 20;
 	WHILE i < tableCount
 	DO
 		SET @STMT := CONCAT("CREATE  TABLE `account_", FORMAT(i, 0), "` (
 					`accountId` INT(10) UNSIGNED NOT NULL ,
+					`applicationId` INT(10) UNSIGNED NOT NULL ,
 					`accountName` VARCHAR(45) NOT NULL ,
 					`nickName` VARCHAR(45) NOT NULL ,
 					`passward` VARCHAR(45) NOT NULL ,
