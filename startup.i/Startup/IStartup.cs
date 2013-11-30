e29d9655733d7ae1608f86753dba17370dcce127
zyouhua <zyouhua@gmail.com>
2013-11-30 17:31:52 +0800

    update

---
+++ startup.i/Startup/IStartup.cs
@@ -0,0 +1,11 @@
+﻿namespace startup.i
+{
+    public interface IStartup
+    {
+        void _runInit();
+
+        void _runStart();
+
+        void _runQuit();
+    }
+}
---
+++ startup.i/Startup/StartupSingleton.cs
@@ -0,0 +1,32 @@
+﻿namespace startup.i
+{
+    public class StartupSingleton
+    {
+        public void _runInit()
+        {
+            mStarup._runInit();
+        }
+
+        public void _runStart()
+        {
+            mStarup._runStart();
+        }
+
+        public void _runQuit()
+        {
+            mStarup._runQuit();
+        }
+
+        public void _setStartup(IStartup nStartup)
+        {
+            mStarup = nStartup;
+        }
+
+        public StartupSingleton()
+        {
+            mStarup = null;
+        }
+
+        IStartup mStarup;
+    }
+}
